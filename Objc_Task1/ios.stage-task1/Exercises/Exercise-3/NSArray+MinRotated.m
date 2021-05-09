301dbfd3f5d0b79f5e5d3f6fe8d31cdabbc1ad9b
Kamran <iskama98@mail.ru>
2021-05-10 00:41:54 +0300

    feat: complete exercise-3

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-3/NSArray+MinRotated.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-3/NSArray+MinRotated.m
@@ -3,7 +3,20 @@
 @implementation NSArray (MinRotated)
 
 - (NSNumber *)minRotated {
-    return nil;
+    
+    if (self.count == 0)return nil;
+    
+    long l = 0, r = self.count - 1;
+    while (r - l > 1){
+        long mid = (r + l) / 2;
+        if (mid > 0 && [self[mid] intValue] < [self[mid - 1] intValue]){
+            return self[mid];
+        }
+        if ([self[mid] intValue] < [self.lastObject intValue])r = mid;
+        else l = mid;
+    }
+    if ([self[l] intValue] < [self[r] intValue])return self[l];
+    else return self[r];
 }
 
 @end

1fc37c4020450f07bf79a77fb6190c63f352eab5
Kamran <iskama98@mail.ru>
2021-05-10 00:45:15 +0300

    feat: complete exercise-4

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
@@ -3,7 +3,24 @@
 @implementation NumberConverter
 
 - (NSArray *)numberConverter:(NSNumber *)number {
-    return nil;
-}
-
+    
+    int x = [number intValue];
+        if (x < 0){
+            x *= -1;
+        }
+        
+        NSMutableArray *array = [[NSMutableArray alloc] init];
+        while (x > 0){
+            
+            int digit = x % 10;
+            x /= 10;
+            
+            [array addObject:[NSString stringWithFormat:@"%d", digit]];
+            
+        }
+        
+        NSArray *finalArray = [[NSArray alloc] initWithArray:array];
+        
+        return finalArray;
+    }
 @end

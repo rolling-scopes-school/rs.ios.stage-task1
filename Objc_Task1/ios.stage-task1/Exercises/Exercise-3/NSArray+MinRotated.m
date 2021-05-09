f69de68a0e1279f4a9fa7dac0f887acf7c87624e
Vladimir <vovabulay@gmail.com>
2021-05-09 11:19:31 +0300

    subtask3: subtask3 completed

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-3/NSArray+MinRotated.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-3/NSArray+MinRotated.m
@@ -3,7 +3,20 @@
 @implementation NSArray (MinRotated)
 
 - (NSNumber *)minRotated {
-    return nil;
+    if (self == nil || self.count == 0) {
+        return nil;
+    }
+    NSInteger min = 0;
+    NSInteger max = self.count - 1;
+    while(min < max) {
+        NSInteger mid = min + (max - min)/2;
+        if([self[mid] intValue] > [self[max] intValue]) {
+            min = mid + 1;
+        } else {
+            max = mid;
+        }
+    }
+    return self[max];
 }
 
 @end

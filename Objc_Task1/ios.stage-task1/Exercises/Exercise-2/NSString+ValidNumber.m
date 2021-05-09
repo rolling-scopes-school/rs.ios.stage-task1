bd5cef0acdd722596c96b45cf54dd355608c8aae
Vladimir <vovabulay@gmail.com>
2021-05-09 11:19:03 +0300

    subtask2: subtask2 completed

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
@@ -3,7 +3,17 @@
 @implementation NSString (ValidNumber)
 
 - (BOOL)isValidNumber {
-    return false;
+    if (self == nil || self.length == 0) {
+        return false;
+    }
+    NSCharacterSet *numericSet = [NSCharacterSet decimalDigitCharacterSet];
+    for (NSInteger i = 0; i < self.length; i++) {
+        unichar c = [self characterAtIndex:i];
+        if (![numericSet characterIsMember:c]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 @end

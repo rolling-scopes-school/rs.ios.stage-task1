7e50c02e868aa41dabf604c8bade9b021eec7c6f
Vladimir <vovabulay@gmail.com>
2021-05-09 11:19:59 +0300

    subtask4: subtask4 completed

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
@@ -3,7 +3,15 @@
 @implementation NumberConverter
 
 - (NSArray *)numberConverter:(NSNumber *)number {
-    return nil;
+    NSInteger num = ABS([number intValue]);
+    NSMutableArray *result = [[NSMutableArray alloc] init];
+    NSInteger i = 0;
+    while (num > 0) {
+        result[i] = [NSString stringWithFormat:@"%ld", num % 10];
+        num = num / 10;
+        i++;
+    }
+    return result;
 }
 
 @end

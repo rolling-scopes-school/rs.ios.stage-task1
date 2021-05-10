d7d0c9e53c5b4a53af58323c8c321191e32f4154
Aleksandr Bucha <aliaksandr.bucha@gmail.com>
2021-05-10 22:28:34 +0300

    feat: add Find the minimum in an rotated sorted array of unique nums with unknown pivot. Exercise-1

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-3/NSArray+MinRotated.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-3/NSArray+MinRotated.m
@@ -3,7 +3,18 @@
 @implementation NSArray (MinRotated)
 
 - (NSNumber *)minRotated {
-    return nil;
+    if (self.count == 0) {
+        return nil;
+    }
+    if (![self isKindOfClass: [NSArray<NSNumber *> class]]) {
+        return nil;
+    }
+    NSMutableArray<NSNumber *> * array = [self mutableCopy];
+    while (array[0].doubleValue > array[array.count - 1].doubleValue) {
+        [array addObject:array[0]];
+        [array removeObjectAtIndex:0];
+    }
+    return array[0];
 }
 
 @end

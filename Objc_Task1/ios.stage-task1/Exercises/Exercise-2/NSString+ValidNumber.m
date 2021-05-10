cb0b617397b16975420fb9932b915e073719d378
Aleksandr Bucha <albu13g@gmail.com>
2021-05-10 16:43:20 +0300

    feat: add Determine if the string represents a valid number. Exercise-1

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
@@ -3,7 +3,32 @@
 @implementation NSString (ValidNumber)
 
 - (BOOL)isValidNumber {
-    return false;
+    if (self.length == 0) {
+        return NO;
+    }
+    
+    NSInteger location = 0;
+    NSInteger chunkSize = 10;
+    while (location < self.length) {
+        NSInteger length = MIN(self.length - location, chunkSize);
+        NSRange range = NSMakeRange(location, length);
+        
+        NSString *substring = [self substringWithRange:range];
+        if ([self numberWithString:substring] == nil) {
+            return NO;
+        }
+        location += chunkSize;
+    }
+    return YES;
+}
+
+- (nullable NSNumber *)numberWithString:(nonnull NSString *)string {
+    if ([string containsString:@" "]) {
+        return nil;
+    }
+    NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
+    formatter.numberStyle = NSNumberFormatterDecimalStyle;
+    return [formatter numberFromString:string];
 }
 
 @end

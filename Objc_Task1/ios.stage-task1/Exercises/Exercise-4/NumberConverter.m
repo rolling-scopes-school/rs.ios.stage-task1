d1801241e81e110dc9c2c6e8758220fd0a05a911
Aleksandr Bucha <aliaksandr.bucha@gmail.com>
2021-05-11 00:33:23 +0300

    feat: add Given a non-negative number, you need to return a characters array of this number in reverse order. Exercise-1

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
@@ -3,7 +3,16 @@
 @implementation NumberConverter
 
 - (NSArray *)numberConverter:(NSNumber *)number {
-    return nil;
+    NSString * str = @(llabs(number.integerValue)).stringValue;
+    NSMutableArray * array = [[NSMutableArray alloc] initWithCapacity:[str length]];
+    NSInteger location = str.length - 1;
+    while (location >= 0) {
+        NSRange range = NSMakeRange(location, 1);
+        NSString *substring = [str substringWithRange:range];
+        [array addObject:substring];
+        location -= 1;
+    }
+    return array;
 }
 
 @end
